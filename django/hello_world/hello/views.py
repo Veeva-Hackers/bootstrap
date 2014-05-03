fa8f6173a61f8ca006c3287f9bb49003f6d03244
Steven Wan <steven.wan@veeva.com>
2014-05-03 15:08:46 -0700

    hook up angular

--- django/hello_world/hello/views.py
+++ django/hello_world/hello/views.py
@@ -12,19 +12,19 @@ def home(request):
 
 
 def search_yelp(request):
-    yelp_api = YelpAPI(consumer_key='iuNELsk9FWsJUHb-LdFx_A', consumer_secret='THc0I88ppFD45fjK_fGnXEhaGKs', token='jv8hIDODrf8kHAIalsFdgaJKSvo86E0U',
-                       token_secret='_zQuQVlWTn_tzfnw_uhAWbQJJIs')
-    search_results = yelp_api.search_query(radius_filter='5', term = 'restaurants', limit = 25)
-
-    for business in search_results.businesses:
-        name =  business.name
-        image_url = business.image_url
-        url = business.url
-        ratings = business.ratings
-        print 'name: ' , name
-
+    # yelp_api = YelpAPI(consumer_key='iuNELsk9FWsJUHb-LdFx_A', consumer_secret='THc0I88ppFD45fjK_fGnXEhaGKs', token='jv8hIDODrf8kHAIalsFdgaJKSvo86E0U',
+    #                    token_secret='_zQuQVlWTn_tzfnw_uhAWbQJJIs')
+    # search_results = yelp_api.search_query(radius_filter='5', term = 'restaurants', limit = 25)
+    #
+    # for business in search_results.businesses:
+    #     name =  business.name
+    #     image_url = business.image_url
+    #     url = business.url
+    #     ratings = business.ratings
+    #     print 'name: ' , name
+    #
     return render_to_response('portfolio.html', {
-        'results': search_results
+        # 'results': search_results
     })
 
 def search_restaurant_name(request):
