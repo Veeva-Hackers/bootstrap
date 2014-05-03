3528a0400d470c4339118768dcea4b1f222d39c6
alan_wang_veeva <alan.wang@veevasystems.com>
2014-05-02 18:14:59 -0700

    Check in hello world template and static file

--- django/hello_world/hello/views.py
+++ django/hello_world/hello/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def home(request):
-
-    return render_to_response('hello/templates/hello.html', {
+    return render_to_response('hello.html', {
         'girum': 'ibssa'
     })
