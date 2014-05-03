fc4692221c2454ae7927ea7ddf2f7376d507e5de
Steven Wan <steven.wan@veeva.com>
2014-05-03 15:42:44 -0700

    hook up angular

--- django/hello_world/static/hello/js/controller.js
+++ django/hello_world/static/hello/js/controller.js
@@ -1,14 +1,9 @@
 var helloApp = angular.module('helloApp', []);
 
 helloApp.controller('resultListCtrl', ['$scope', '$http',
-  function ($scope) {
-      $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
-          $scope.restaurants = [
-            {'name': 'Round Table'},
-              {'name': 'McDonalds'},
-              {'name': 'Burger King'},
-              {'name': 'Carls Jr'}
-          ];
+  function ($scope, $http) {
+      $http.get('/searchrestaurant').success(function(data) {
+          $scope.restaurants = data;
     });
 }]);
 
