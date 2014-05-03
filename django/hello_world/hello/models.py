b4170755658d17b562ea68e4431701695132dd3f
brent <brent.arata@gmail.com>
2014-05-03 11:41:58 -0700

    Increased the field length of themodel

--- django/hello_world/hello/models.py
+++ django/hello_world/hello/models.py
@@ -1,3 +1,5 @@
+import io
+
 from django.db import models
 
 class Restaurant(models.Model):
@@ -7,4 +9,10 @@ class Restaurant(models.Model):
     facility_name = models.CharField(max_length=300)
     violation_description = models.CharField(max_length=1000)
 
+def parseFromFlatFile():
+    with io.open('restraurantinspect.json', 'r'):
+        pass
 
+def load():
+    for restaurant in []:
+        pass
