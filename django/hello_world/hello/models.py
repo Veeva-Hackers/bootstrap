e2ce2e5a67bdb2b490dd84b45801f2a2a8b53c31
alan_wang_veeva <alan.wang@veevasystems.com>
2014-05-03 11:58:04 -0700

    Setup better path for restarant json

--- django/hello_world/hello/models.py
+++ django/hello_world/hello/models.py
@@ -1,4 +1,7 @@
 import io
+import json
+import os
+from hello_world import settings
 
 from django.db import models
 
@@ -10,8 +13,8 @@ class Restaurant(models.Model):
     violation_description = models.CharField(max_length=1000)
 
 def parseFromFlatFile():
-    with io.open('restraurantinspect.json', 'r'):
-        pass
+    with io.open(os.path.join(settings.BASE_DIR, 'static/restaurantinspect.json'), 'r') as file:
+        print file
 
 def load():
     for restaurant in []:
