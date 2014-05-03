0e511cb28a6aa4f4726b642faad8f08a4ecfcbc9
brent <brent.arata@gmail.com>
2014-05-03 11:40:09 -0700

    Made the fields for various fields larger

--- django/hello_world/hello/models.py
+++ django/hello_world/hello/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Restaurant(models.Model):
     activity_date = models.CharField(max_length=30)
-    resource_code = models.CharField(max_length=30)
-    human_address = models.CharField(max_length=30)
-    facility_name = models.CharField(max_length=30)
-    violation_description = models.CharField(max_length=30)
+    resource_code = models.CharField(max_length=100)
+    human_address = models.CharField(max_length=300)
+    facility_name = models.CharField(max_length=300)
+    violation_description = models.CharField(max_length=1000)
 
 
