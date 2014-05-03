6f1273f9f4ea284fa2aad3c1ed8db44f2ed0789b
Girum Ibssa <ibssagirum@gmail.com>
2014-05-02 23:05:39 -0700

    Django-ified the HTML templates. Homepage works via Django homepage, but all other pages still need to be opened statically by opening the index.html file. Bring on the hackathon~~  B-)

--- django/hello_world/hello/views.py
+++ django/hello_world/hello/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def home(request):
-    return render_to_response('hello.html', {
+    return render_to_response('index.html', {
         'girum': 'ibssa'
     })
