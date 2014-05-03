112e43b4ec881b176132b01f90da06e72ca17f85
Girum Ibssa <ibssagirum@gmail.com>
2014-05-03 15:10:25 -0700

    Working on yelp

--- django/hello_world/hello/views.py
+++ django/hello_world/hello/views.py
@@ -14,20 +14,24 @@ def home(request):
 
 
 def search_yelp(request):
-    # yelp_api = YelpAPI(consumer_key='iuNELsk9FWsJUHb-LdFx_A', consumer_secret='THc0I88ppFD45fjK_fGnXEhaGKs', token='jv8hIDODrf8kHAIalsFdgaJKSvo86E0U',
-    #                    token_secret='_zQuQVlWTn_tzfnw_uhAWbQJJIs')
-    # search_results = yelp_api.search_query(radius_filter='5', term = 'restaurants', limit = 25)
+    yelp_api = YelpAPI(consumer_key='iuNELsk9FWsJUHb-LdFx_A',
+                       consumer_secret='THc0I88ppFD45fjK_fGnXEhaGKs',
+                       token='jv8hIDODrf8kHAIalsFdgaJKSvo86E0U',
+                       token_secret='_zQuQVlWTn_tzfnw_uhAWbQJJIs')
+
+    # search_results = yelp_api.search_query(radius_filter='5', term='restaurants', limit=25)
     #
     # for business in search_results.businesses:
-    #     name =  business.name
+    #     name = business.name
     #     image_url = business.image_url
     #     url = business.url
     #     ratings = business.ratings
-    #     print 'name: ' , name
-    #
+    #     print 'name: ', name
+
     return render_to_response('portfolio.html', {
         # 'results': search_results
     })
+
 
 def search_restaurant_name(request):
     q = '1'
@@ -36,6 +40,7 @@ def search_restaurant_name(request):
 
     serializer = RestaurantSerializer(matching_restaurants)
     return HttpResponse(JSONRenderer().render(serializer.data))
+
 
 def initialize_db(request):
     models.load()
